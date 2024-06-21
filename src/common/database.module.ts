57d6325f42a9a1e6b884d99a8784be118078fdff
black02430horse <black02430horse@gmail.com>
2024-06-21 19:23:06 +0200

    docker

--- src/common/database.module.ts
+++ src/common/database.module.ts
@@ -7,12 +7,12 @@ dotenv.config();
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
+      type: 'postgres',
       host: process.env.DB_HOST,
       port: parseInt(process.env.DB_PORT, 10),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
       entities: [__dirname + '/../**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
