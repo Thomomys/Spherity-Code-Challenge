4db6149ad3421ec9c488e304ef88f4fc35c04c40
black02430horse <black02430horse@gmail.com>
2024-06-21 17:32:38 +0200

    fix:lint and format

--- src/secret-note/secret-note.entity.ts
+++ src/secret-note/secret-note.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn, Timestamp } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 import { ApiProperty } from '@nestjs/swagger';
 
 @Entity()
